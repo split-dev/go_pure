8ea4eec9f289f4fe4c258808d86b7b5fb1daa018
vlad <vkravchenko@splitdev.pro>
2021-03-09 19:19:52 +0200

    faq almosts done

--- assets/common.js
+++ assets/common.js
@@ -1,19 +1,48 @@
 'use strict';
 
-var body = document.body;
-var burger = document.querySelector('[data-menu-toggle]');
-var mobMenu = document.querySelector('[data-mob-menu]');
+var page = page || {};
 
+page.body = document.body;
+page.header = document.querySelector('.header');
+page.burger = document.querySelector('[data-menu-toggle]');
+page.mobMenu = document.querySelector('[data-mob-menu]');
+
+//Lazyload page
 document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function () {
-        body.style.opacity = '1';
+        page.body.style.opacity = '1';
     }, 200);
 });
 
-burger.addEventListener('click', function () {
-    mobMenu.classList.toggle('open');
+//Mob Nav Actions
+page.burger.addEventListener('click', function () {
+    page.mobMenu.classList.toggle('open');
 
-    burger.querySelectorAll('span').forEach(function (icon) {
+    page.burger.querySelectorAll('span').forEach(function (icon) {
         icon.classList.toggle('d-none');
+    });
+});
+
+//* Sections
+//> Page Hero
+document.querySelector('[data-scroll-down]').addEventListener('click', function (e) {
+    var scrollHeight = e.target.closest('section').offsetHeight;
+
+    // Checking for additional offset created by header
+    var announcementBar = page.header.querySelector('.header__bar');
+    if (announcementBar) {
+        scrollHeight += announcementBar.offsetHeight;
+    }
+
+    document.documentElement.scrollTop = scrollHeight; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = scrollHeight; // For Safari
+});
+
+//> Accordions
+document.querySelectorAll('[data-accordion]').forEach(function (accordion) {
+    accordion.addEventListener('click', function () {
+        accordion.classList.toggle('active');
+        var panel = accordion.nextElementSibling;
+        panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + "px";
     });
 });
--- dev/js/common.js
+++ dev/js/common.js
@@ -1,18 +1,52 @@
-const body = document.body;
-const burger = document.querySelector('[data-menu-toggle]');
-const mobMenu = document.querySelector('[data-mob-menu]');
+const page = page || {};
 
+page.body = document.body;
+page.header = document.querySelector('.header');
+page.burger = document.querySelector('[data-menu-toggle]');
+page.mobMenu = document.querySelector('[data-mob-menu]');
+
+//Lazyload page
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
-        body.style.opacity = '1';
+        page.body.style.opacity = '1';
     },200);
 });
 
-burger.addEventListener('click', () => {
-    mobMenu.classList.toggle('open');
+//Mob Nav Actions
+page.burger.addEventListener('click', () => {
+    page.mobMenu.classList.toggle('open');
 
-    burger.querySelectorAll('span')
+    page.burger.querySelectorAll('span')
         .forEach((icon) => {
             icon.classList.toggle('d-none');
         });
 });
+
+//* Sections
+//> Page Hero
+document.querySelector('[data-scroll-down]')
+    .addEventListener('click', (e) => {
+        let scrollHeight = e.target.closest('section').offsetHeight;
+
+        // Checking for additional offset created by header
+        let announcementBar = page.header.querySelector('.header__bar');
+        if (announcementBar) {
+            scrollHeight += announcementBar.offsetHeight;
+        }
+
+        document.documentElement.scrollTop = scrollHeight; // For Chrome, Firefox, IE and Opera
+        document.body.scrollTop = scrollHeight;  // For Safari
+});
+
+//> Accordions
+document.querySelectorAll('[data-accordion]')
+    .forEach((accordion) => {
+        accordion.addEventListener('click', () => {
+            accordion.classList.toggle('active');
+            let panel = accordion.nextElementSibling;
+            panel.style.maxHeight
+                ? panel.style.maxHeight = null
+                : panel.style.maxHeight = panel.scrollHeight + "px"
+        });
+    }
+);
